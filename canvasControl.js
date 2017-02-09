9422de4f188575c9d94b3756b3a312390027b97c
wolves13 <m116175@gmail.com>
2017-02-09 15:44:15 +0900

    2nd commit

--- canvasControl.js
+++ canvasControl.js
@@ -106,7 +106,7 @@ var drawBeadType = function(x, y, beadType, context, color){
     var text = beadType.toString();
     var centerP = getPointOnOCS(x, y);
 
-    context.font = '2px century';               // ここでbeadType のfontを決定してる.
+    context.font = '15px century';               // ここでbeadType のfontを決定してる.
     context.textBaseline = 'middle';    
     context.textAlign = 'center';
     context.fillStyle = color ? color : 'black';
